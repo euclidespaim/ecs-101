47bed2ca6449a17e6b35dbd4f292e8ab3f14881d
Euclides Paim <euclidespaim@gmail.com>
2026-06-15 10:03:53 -0300

    Testes condicionais em python

---
+++ Logica de Programação/1-Teoria/aula-1/aula01.py
@@ -0,0 +1,22 @@
+'''
+O que é o IF em Python?
+
+O if é usado para executar um bloco de código 
+ apenas se uma determinada condição for verdadeira.
+
+Exemplo:
+'''
+
+a = 10
+b = 20
+
+if a < b:
+    print("A é menor que B")
+
+
+
+
+
+
+
+
---
+++ Logica de Programação/1-Teoria/aula-1/aula02.py
@@ -0,0 +1,10 @@
+'''
+Cálculo da Média de Notas
+Solicite ao usuário as notas de 2 disciplinas e calcule a média. 
+
+Se a média for maior ou igual a 7, informe "Aprovado". 
+Caso contrário, informe "Reprovado".
+'''
+
+
+
